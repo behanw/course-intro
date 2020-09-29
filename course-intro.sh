995090ffde8061e4d16054203c750d52c9711d11
Behan Webster <behanw@converseincode.com>
2020-09-28 23:30:29 -0700

    Reorganize get routines in course-intro.sh
    Signed-off-by: Behan Webster <behanw@converseincode.com>

--- course-intro.sh
+++ course-intro.sh
@@ -19,6 +19,7 @@ CMD="$(basename "$0")"
 CACHEDIR="$HOME/.cache/${CMD%.sh}"
 CONFIGDIR="$HOME/.config/${CMD%.sh}"
 CONF="$CONFIGDIR/settings.conf"
+CONFTEX="settings.tex"
 MYPID="$$"
 
 #===============================================================================
@@ -90,21 +91,22 @@ BACK="\e[0m"
 
 ################################################################################
 metadata() {
-	info "Instructor:'$INSTRUCTOR'"
-	info "Email:     '$EMAIL'"
-	info "Date:      '$DATE'"
-	info "OpenEnrol: '$OPENENROL'"
-	info "Company:   '$COMPANY'"
-	info "InPerson:  '$INPERSON'"
-	info "Location:  '$LOCATION'"
-	info "Course:    '$COURSE'"
-	info "Title:     '$TITLE'"
-	info "Revision:  '$REVISION'"
-	info "Time:      '$TIME'"
-	info "TimeZone:  '$ZONE'"
-	info "Key:       '$KEY'"
-	info "Eval:      '$EVAL'"
-	info "Bitly Link:'$BITLY_LINK'"
+	info "Metadata for this course:"
+	info "  Instructor:'$INSTRUCTOR'"
+	info "  Email:     '$EMAIL'"
+	info "  Date:      '$DATE'"
+	info "  OpenEnrol: '$OPENENROL'"
+	info "  Company:   '$COMPANY'"
+	info "  InPerson:  '$INPERSON'"
+	info "  Location:  '$LOCATION'"
+	info "  Course:    '$COURSE'"
+	info "  Title:     '$TITLE'"
+	info "  Revision:  '$REVISION'"
+	info "  Time:      '$TIME'"
+	info "  TimeZone:  '$ZONE'"
+	info "  Key:       '$KEY'"
+	info "  Eval:      '$EVAL'"
+	info "  Bitly Link:'$BITLY_LINK'"
 
 	add_json "$METAFILE" "$JSON_DATE" "$DATE"
 	add_json "$METAFILE" "$JSON_COMPANY" "$COMPANY"
@@ -139,22 +141,6 @@ error() {
 	echo -e "${RED}E:" "$@" "$BACK" >&2
 	kill -9 "$MYPID"
 	exit 1
-}
-
-################################################################################
-check_git_updates() {
-	if [[ -z $NOUPDATE && -d $TEMPLATE/.git ]] ; then
-		info "Checking for available updates"
-		(cd $TEMPLATE
-		git remote update >/dev/null
-		if ! git status -uno | grep -q "Your branch is up to date" ; then
-			if [[ -z $UPDATE ]] ; then
-				warn "There is an update for CMD (Use --update to update)"
-			else
-				$TEST git pull >/dev/null
-			fi
-		fi)
-	fi
 }
 
 ################################################################################
@@ -230,65 +216,6 @@ query_json() {
 }
 
 ################################################################################
-gettitle() {
-	local STR=$1 
-
-	if [[ -z $TITLE && -e $METAFILE ]] ; then
-		TITLE="$(read_json "$METAFILE" "$JSON_TITLE")"
-	fi
-	if [[ -z $TITLE ]] && command -v "$READYFOR" >/dev/null ; then
-		TITLE="$("$READYFOR" -l 2>/dev/null \
-			| grep "$STR" | sed -r -e "s/ *$STR - //")"
-	fi
-	if [[ -z $TITLE ]] ; then
-		TITLE="$(query_json ".activities | .$COURSE | .title")"
-	fi
-	add_json "$METAFILE" "$JSON_TITLE" "$TITLE"
-}
-
-################################################################################
-getcourse() {
-	local STR=$1 NAME
-
-	NAME="$(sed -r 's/^.*(LF[A-Z][0-9]+).*$/\1/' <<<"$STR")"
-	if [[ -z $COURSE && -n $NAME ]] ; then
-		COURSE="$NAME"
-		gettitle "$COURSE"
-	fi
-}
-
-################################################################################
-getrevision() {
-	local STR=$1 REV
-
-	REV="$(sed -r 's/^.*(v[0-9.]+).*$/\1/' <<<"$STR")"
-	if [[ -n $REV && -z $REVISION ]] ; then
-		REVISION="$(sed -e 's/^v//I' -e 's/^/V/' <<<"$REV")"
-		debug "getrevison: REVISION=$REVISION"
-	fi
-}
-
-################################################################################
-getcm() {
-	local FILES
-	FILES="$(read_json "$METAFILE" "$JSON_FILES")"
-	[[ -n $FILES ]] || FILES="$(query_json ".activities | .$COURSE | .materials | .[]")"
-	[[ -z $FILES ]] || add_json "$METAFILE" "$JSON_FILES" "$FILES"
-	echo "$FILES"
-}
-
-################################################################################
-getkey() {
-	local STR=$1 CODE
-
-	CODE="$(sed -r 's/^.*: *//' <<<"$STR")"
-	debug "getkey: $STR -> $CODE"
-	if [[ -n $CODE && $CODE =~ ^[0-9A-Za-z]+$ ]] ; then
-		KEY="$CODE"
-	fi
-}
-
-################################################################################
 getbitly() {
 	local URL=$1 API AUTH TYPE JSON
 	API='https://api-ssl.bitly.com/v4/shorten'
@@ -312,12 +239,68 @@ getbitly() {
 }
 
 ################################################################################
+getcm() {
+	local FILES
+	FILES="$(read_json "$METAFILE" "$JSON_FILES")"
+	[[ -n $FILES ]] || FILES="$(query_json ".activities | .$COURSE | .materials | .[]")"
+	[[ -z $FILES ]] || add_json "$METAFILE" "$JSON_FILES" "$FILES"
+	echo "$FILES"
+}
+
+################################################################################
+getcourse() {
+	local STR=$1 NAME
+
+	NAME="$(sed -r 's/^.*(LF[A-Z][0-9]+).*$/\1/' <<<"$STR")"
+	if [[ -z $COURSE && -n $NAME ]] ; then
+		COURSE="$NAME"
+		gettitle "$COURSE"
+	fi
+}
+
+################################################################################
+getdate() {
+	local STR=$1
+
+	if [[ $STR =~ ^[0-9.]{8,10} && -z $DATE ]] ; then
+		DATE="$STR"
+		debug "  Detecting date as $DATE"
+	fi
+}
+
+################################################################################
 getevaluation() {
 	local STR=$1
 
 	EVAL="$(sed -r 's/^.*http/http/' <<<"$STR")"
 	if [[ -n $EVAL ]] ; then
 		[[ $EVAL =~ https?:// ]] || error "Invalid eval URL: '$EVAL'"
+	fi
+}
+
+################################################################################
+getkey() {
+	local STR=$1 CODE
+
+	CODE="$(sed -r 's/^.*: *//' <<<"$STR")"
+	debug "getkey: $STR -> $CODE"
+	if [[ -n $CODE && $CODE =~ ^[0-9A-Za-z]+$ ]] ; then
+		KEY="$CODE"
+	fi
+}
+
+################################################################################
+getlocation() {
+	LOC="$(read_json "$METAFILE" "$JSON_LOC")"
+	if [[ -n $LOC && $LOC != null ]] ; then
+		LOCATION="$LOC"
+		if [[ $LOC = "Virtual" ]] ; then
+			INPERSON=n
+			debug "  Detecting Virtual class"
+		else
+			INPERSON=y
+			debug "  Detecting In-person class in $LOC"
+		fi
 	fi
 }
 
@@ -337,18 +320,66 @@ getopenenrol() {
 }
 
 ################################################################################
-getlocation() {
-	LOC="$(read_json "$METAFILE" "$JSON_LOC")"
-	if [[ -n $LOC && $LOC != null ]] ; then
-		LOCATION="$LOC"
-		if [[ $LOC = "Virtual" ]] ; then
-			INPERSON=n
-			debug "  Detecting Virtual class"
-		else
-			INPERSON=y
-			debug "  Detecting In-person class in $LOC"
-		fi
+getrevision() {
+	local STR=$1 REV
+
+	REV="$(sed -r 's/^.*(v[0-9.]+).*$/\1/' <<<"$STR")"
+	if [[ -n $REV && -z $REVISION ]] ; then
+		REVISION="$(sed -e 's/^v//I' -e 's/^/V/' <<<"$REV")"
+		debug "getrevision: REVISION=$REVISION"
 	fi
+}
+
+################################################################################
+gettitle() {
+	local STR=$1 
+
+	if [[ -z $TITLE && -e $METAFILE ]] ; then
+		TITLE="$(read_json "$METAFILE" "$JSON_TITLE")"
+	fi
+	if [[ -z $TITLE ]] && command -v "$READYFOR" >/dev/null ; then
+		TITLE="$("$READYFOR" -l 2>/dev/null \
+			| grep "$STR" | sed -r -e "s/ *$STR - //")"
+	fi
+	if [[ -z $TITLE ]] ; then
+		TITLE="$(query_json ".activities | .$COURSE | .title")"
+	fi
+	add_json "$METAFILE" "$JSON_TITLE" "$TITLE"
+}
+
+################################################################################
+check_git_updates() {
+	if [[ -z $NOUPDATE && -d $TEMPLATE/.git ]] ; then
+		info "Checking for available updates"
+		(cd $TEMPLATE
+		git remote update >/dev/null
+		if ! git status -uno | grep -q "Your branch is up to date" ; then
+			if [[ -z $UPDATE ]] ; then
+				warn "There is an update for CMD (Use --update to update)"
+			else
+				$TEST git pull >/dev/null
+				warn "Updated $CMD"
+				exit 0
+			fi
+		fi)
+	fi
+}
+
+################################################################################
+getdata_json() {
+	[[ -f "$METAFILE" ]] || return 0
+	info "Reading info from './$METAFILE'"
+
+	[[ -n $DATE ]] || getdate "$(read_json "$METAFILE" "$JSON_DATE" 'Date')"
+	[[ -n $COURSE ]] || getcourse "$(read_json "$METAFILE" "$JSON_CODE")"
+	[[ -n $REVISION ]] || getrevision "$(read_json "$METAFILE" "$JSON_VERSION")"
+	[[ -n $TIME ]] || TIME="$(read_json "$METAFILE" "$JSON_TIME")"
+	[[ -n $ZONE ]] || ZONE="$(read_json "$METAFILE" "$JSON_ZONE")"
+	[[ -n $KEY ]] || getkey "$(read_json "$METAFILE" "$JSON_KEY")"
+	[[ -n $EVAL ]] || getevaluation "$(read_json "$METAFILE" "$JSON_SURVEY")"
+
+	getopenenrol "$(read_json "$METAFILE" "$JSON_COMPANY")"
+	getlocation "$(read_json "$METAFILE" "$JSON_LOC")"
 }
 
 ################################################################################
@@ -370,23 +401,20 @@ getdata_dir() {
 	debug "getdata: DATE:$DATE COURSE:$COURSE KEY:$KEY EVAL:$EVAL OTHER:$OTHER"
 
 	#-----------------------------------------------------------------------
-	if [[ $NDATE =~ ^[0-9]{2} ]] ; then
-		if [[ -z $DATE ]] ; then
-			info "Reading info from '$NAME'"
-			DATE="$NDATE"
-			warn "  Detecting date as $DATE"
-		fi
-	else
-		dir_warning
-	fi
-
-	#-----------------------------------------------------------------------
 	if [[ -n $NCOURSE ]] ; then
+		info "Reading info from '$NAME'"
 		getcourse "$NCOURSE"
 	else
 		dir_warning
 	fi
 	[[ -n $COURSE ]] || return 0
+
+	#-----------------------------------------------------------------------
+	if [[ $NDATE =~ ^[0-9.]{8,}$ ]] ; then
+		getdate "$NDATE"
+	else
+		dir_warning
+	fi
 
 	#-----------------------------------------------------------------------
 	getopenenrol "$CORP"
@@ -396,21 +424,40 @@ getdata_dir() {
 }
 
 ################################################################################
+getdata_pdf() {
+	local FILE=$1 LINE
+	local FILE="${1:-$CODEPDF}" LINE
+	[[ $FILE =~ pdf$ && -f $FILE ]] || return 0
+	info "Reading info from './$FILE'"
+
+	while read -r LINE ; do
+		case "$LINE" in
+			*Subject:*LF*) getcourse "$LINE";;
+			*Book:*v[0-9]*) getrevision "$LINE";;
+			*Version:*v[0-9]*) getrevision "$LINE";;
+			*Reg*:*) getkey "$LINE";;
+			*Survey*:*) getevaluation "$LINE";;
+			*Evaluation:*) getevaluation "$LINE";;
+		esac
+	done <<<"$(pdfgrep . "$FILE")"
+}
+
+################################################################################
 ymd_to_mdy() {
-	local DATE=$1 Y M D MDY
-	[[ -n $DATE ]] || error "No date found (use --date to fix)"
-	if [[ $DATE =~ \. ]] ; then
-		IFS=. read -r Y M D <<<"$DATE"
-	elif [[ $DATE =~ - ]] ; then
-		IFS=- read -r Y M D <<<"$DATE"
-	elif [[ $DATE =~ / ]] ; then
-		IFS=- read -r M D Y <<<"$DATE"
-	elif [[ $DATE =~ 20[0-9]{6} ]] ; then
-		IFS=- read -r Y M D <<<"$(sed -re 's/([0-9]{4})([0-9]{2})([0-9]{2})/\1-\2-\3/' <<<"$DATE")"
+	local YMD=$1 Y M D MDY
+	[[ -n $YMD ]] || error "No date found (use --date to fix)"
+	if [[ $YMD =~ \. ]] ; then
+		IFS=. read -r Y M D <<<"$YMD"
+	elif [[ $YMD =~ - ]] ; then
+		IFS=- read -r Y M D <<<"$YMD"
+	elif [[ $YMD =~ / ]] ; then
+		IFS=- read -r M D Y <<<"$YMD"
+	elif [[ $YMD =~ 20[0-9]{6} ]] ; then
+		IFS=- read -r Y M D <<<"$(sed -re 's/([0-9]{4})([0-9]{2})([0-9]{2})/\1-\2-\3/' <<<"$YMD")"
 	fi
-	[[ -n $D ]] || error "Date format not recognized: $DATE"
+	[[ -n $D ]] || error "Date format not recognized: $YMD"
 	MDY="${M#0}/${D#0}/$Y"
-	debug "ymd_to_mdy: DATE:$DATE Y:$Y M:$M D:$D MDY:$MDY"
+	debug "ymd_to_mdy: DATE:$YMD Y:$Y M:$M D:$D MDY:$MDY"
 	echo "$MDY"
 }
 
@@ -418,7 +465,7 @@ ymd_to_mdy() {
 getdata_csv() {
 	local FILE="${1:-$ROSTER}" MDY
 	[[ $FILE =~ csv$ && -f $FILE ]] || return 0
-	warn "Reading info from '$FILE'"
+	info "Reading info from '$FILE'"
 
 	MDY="$(ymd_to_mdy "$DATE")"
 
@@ -432,42 +479,6 @@ getdata_csv() {
 	[[ -n $REVISION ]] || getrevision "$R"
 	[[ -n $KEY ]] || getkey "$K"
 	[[ -n $EVAL ]] || getevaluation "$E"
-}
-
-################################################################################
-getdata_json() {
-	[[ -f "$METAFILE" ]] || return 0
-	warn "Reading info from '$METAFILE'"
-
-	[[ -n $DATE ]] || DATE="$(read_json "$METAFILE" "$JSON_DATE" 'Date')"
-	[[ -n $COURSE ]] || getcourse "$(read_json "$METAFILE" "$JSON_CODE")"
-	[[ -n $REVISION ]] || getrevision "$(read_json "$METAFILE" "$JSON_VERSION")"
-	[[ -n $TIME ]] || TIME="$(read_json "$METAFILE" "$JSON_TIME")"
-	[[ -n $ZONE ]] || ZONE="$(read_json "$METAFILE" "$JSON_ZONE")"
-	[[ -n $KEY ]] || getkey "$(read_json "$METAFILE" "$JSON_KEY")"
-	[[ -n $EVAL ]] || getevaluation "$(read_json "$METAFILE" "$JSON_SURVEY")"
-
-	getopenenrol "$(read_json "$METAFILE" "$JSON_COMPANY")"
-	getlocation "$(read_json "$METAFILE" "$JSON_LOC")"
-}
-
-################################################################################
-getdata_pdf() {
-	local FILE=$1 LINE
-	local FILE="${1:-$CODEPDF}" LINE
-	[[ $FILE =~ pdf$ && -f $FILE ]] || return 0
-	warn "Reading info from '$FILE'"
-
-	while read -r LINE ; do
-		case "$LINE" in
-			*Subject:*LF*) getcourse "$LINE";;
-			*Book:*v[0-9]*) getrevision "$LINE";;
-			*Version:*v[0-9]*) getrevision "$LINE";;
-			*Reg*:*) getkey "$LINE";;
-			*Survey*:*) getevaluation "$LINE";;
-			*Evaluation:*) getevaluation "$LINE";;
-		esac
-	done <<<"$(pdfgrep . "$FILE")"
 }
 
 ################################################################################
@@ -606,6 +617,29 @@ showpdf() {
 }
 
 ################################################################################
+read_config() {
+	# Read config file
+	if [[ -e $CONF ]] ; then
+		info "Reading config from '$CONF'"
+		# shellcheck disable=SC1090
+		source "$CONF"
+	fi
+	if [[ -e $LOCALCONF ]] ; then
+		info "Reading config from './$LOCALCONF'"
+		# shellcheck disable=SC1090
+		source "$LOCALCONF"
+	fi
+
+	# Link tex config file
+	[[ -n $TEMPLATE ]] || error "No TEMPLATE specified in $CONF"
+	if [[ ! -e "$TEMPLATE/$CONFTEX" ]] ; then
+		warn "Creating $TEMPLATE/$CONFTEX"
+		rm -f "$TEMPLATE/$CONFTEX"
+		ln -s "$CONFIGDIR/$CONFTEX" "$TEMPLATE/$CONFTEX"
+	fi
+}
+
+################################################################################
 usage() {
 	if [[ $# -gt 0 ]] ; then
 		echo -e "${RED}E: Invalid argument:" "$@" "$BACK"
@@ -659,7 +693,7 @@ parse_args() {
 		case "$1" in
 			-c|--cou*) shift; getcourse "$1";;
 			-C|--copy) COPY=y;;
-			-d|--date*) shift; DATE="$1";;
+			-d|--date*) shift; getdate "$1";;
 			-D|--debug) DEBUG=y;;
 			-e|--eval*) shift; getevaluation "$1";;
 			-f|--file*) shift; FILE="$1";;
@@ -691,19 +725,7 @@ parse_args() {
 }
 
 ################################################################################
-if [[ -e $CONF ]] ; then
-	# shellcheck disable=SC1090
-	source "$CONF"
-fi
-[[ -n $TEMPLATE ]] || error "No TEMPLATE specified in $CONF"
-SETTINGS="${SETTINGS:-settings.tex}"
-if [[ ! -e "$TEMPLATE/$SETTINGS" ]] ; then
-	warn "Creating $TEMPLATE/$SETTINGS"
-	rm -f "$TEMPLATE/$SETTINGS"
-	ln -s "$CONFIGDIR/$SETTINGS" "$TEMPLATE/settings.tex"
-fi
-
-################################################################################
+read_config
 parse_args "$@"
 check_git_updates
 getdata_json
@@ -711,11 +733,6 @@ getdata_dir
 getdata_pdf "$FILE"
 getdata_csv "$FILE"
 getdata_other
-if [[ -e $LOCALCONF ]] ; then
-	warn "Reading info from '$LOCALCONF'"
-	# shellcheck disable=SC1090
-	source "$LOCALCONF"
-fi
 [[ -n $QUIET ]] || metadata
 
 ################################################################################
